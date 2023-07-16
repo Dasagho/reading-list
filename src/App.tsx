73dd427b30553e3b9a7199825ee8b5dc07957a77
david <davidamir03@gmail.com>
2023-07-16 19:57:50 +0200

    create root element

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { BookGrid } from './components/BookGrid/BookGrid'
+import { BookList } from './components/BookList/BookList'
+import { type BooksDristribution } from './model/book'
+import BookService from './services/books'
+import styles from './App.module.css'
+import { useLocalStorage } from './hooks/useLocalStorage'
+import { useEffect } from 'react'
+
+function getBooksDistribution (): BooksDristribution {
+  return {
+    library: new BookService().getBooks().library,
+    bookList: []
+  }
+}
+
+export const App: React.FC = () => {
+  const [allBooks, setAllBooks] = useLocalStorage('bookData', getBooksDistribution())
+
+  useEffect(() => {
+    const handleStorageChange = (event: StorageEvent): void => {
+      const item = window.localStorage.getItem('bookData')
+      if (item !== null) {
+        setAllBooks(JSON.parse(item))
+      }
+    }
+
+    window.addEventListener('storage', handleStorageChange)
+
+    return () => {
+      window.removeEventListener('storage', handleStorageChange)
+    }
+  }, [])
+
+  return (
+    <main
+      className={styles.main}
+    >
+      <BookGrid library={allBooks} setBookList={setAllBooks} />
+      <BookList bookList={allBooks} setBookList={setAllBooks} />
+    </main>
+  )
+}
